779d316d40a69b91095d2ff0d182c0bd1eb95d0f
KatyaKlepcha <solianchik@yandex.ru>
2024-01-26 13:15:39 +0300

    feat: create forgot-password form

---
+++ src/components/auth/forgot-password/forgot-password.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ForgotPassword } from './forgot-password'
+
+const meta = {
+  component: ForgotPassword,
+  tags: ['autodocs'],
+  title: 'Auth/ForgotPassword',
+} satisfies Meta<typeof ForgotPassword>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const ForgotPasswordStory: Story = {
+  args: {
+    onSubmit: data => console.info(data),
+  },
+}
